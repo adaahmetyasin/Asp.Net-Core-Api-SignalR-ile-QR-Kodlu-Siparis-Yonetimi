96e83c3f8bce2136cb27b8bc4cc4023dfeb90dae
Yasin Ada <adaahmetyasinedu@gmail.com>
2025-05-07 01:06:52 +0300

    Data access katmanı tamamlandı

---
+++ SignalR.EntityLayer/Entities/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace SignalR.EntityLayer.Entities
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public decimal ProductPrice { get; set; }
+        public bool ProductStatus { get; set; }
+        public string ProductIMG { get; set; }
+    }
+}
