91fca3a3fccd2e314adecd2f8adb8fafd77e49d8
Yasin Ada <adaahmetyasinedu@gmail.com>
2025-05-14 15:03:45 +0300

    contact ve discount controller eklendi

---
+++ SignalRApi/Controllers/ContactController.cs
@@ -0,0 +1,79 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.ContactDto;
+using SignalR.DtoLayer.ContactDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        private readonly IContactService _contactService;
+        private readonly IMapper _mapper;
+
+        public ContactController(IContactService contactService, IMapper mapper)
+        {
+            _contactService = contactService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+
+        public IActionResult ContactList()
+        {
+            var value = _mapper.Map<List<ResultContactDto>>(_contactService.TGetAll());
+            return Ok(value);
+        }
+
+        [HttpPost]
+
+        public IActionResult CreateContact(CreateContactDto createContactDto)
+        {
+            _contactService.TInsert(new Contact()
+            {
+               
+                Location = createContactDto.Location,
+                Phone = createContactDto.Phone,
+                Mail = createContactDto.Mail
+                
+
+            });
+            return Ok("Contact Added");
+        }
+
+        [HttpDelete]
+
+        public IActionResult DeleteContact(int id)
+        {
+            var value = _contactService.TGetById(id);
+            _contactService.TDelete(value);
+            return Ok("Contact Deleted");
+        }
+
+        [HttpGet("GetContact")]
+
+        public IActionResult GetContact(int id)
+        {
+            var value = _contactService.TGetById(id);
+            return Ok(value);
+        }
+
+        [HttpPut]
+
+        public IActionResult UpdateContact(UpdateContactDto updateContactDto)
+        {
+            _contactService.TUpdate(new Contact()
+            {
+                Mail = updateContactDto.Mail,
+                Location = updateContactDto.Location,
+                Phone = updateContactDto.Phone,
+                ContactID = updateContactDto.ContactID
+            });
+            return Ok("Contact Updated");
+        }
+    }
+}
---
+++ SignalRApi/Controllers/DiscountController.cs
@@ -0,0 +1,79 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.DiscountDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiscountController : ControllerBase
+    {
+        private readonly IDiscountService _discountService;
+        private readonly IMapper _mapper;
+
+        public DiscountController(IDiscountService discountService, IMapper mapper)
+        {
+            _discountService = discountService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+
+        public IActionResult DiscountList()
+        {
+            var value = _mapper.Map<List<ResultDiscountDto>>(_discountService.TGetAll());
+            return Ok(value);
+        }
+
+        [HttpPost]
+
+        public IActionResult CreateDiscount(CreateDiscountDto createDiscountDto)
+        {
+            _discountService.TInsert(new Discount()
+            {
+                Title = createDiscountDto.Title,
+                Amount = createDiscountDto.Amount,
+                Description = createDiscountDto.Description,
+                ImageUrl = createDiscountDto.ImageUrl
+
+            });
+            return Ok("Discount Added");
+        }
+
+        [HttpDelete]
+
+        public IActionResult DeleteDiscount(int id)
+        {
+            var value = _discountService.TGetById(id);
+            _discountService.TDelete(value);
+            return Ok("Discount Deleted");
+        }
+
+        [HttpGet("GetDiscount")]
+
+        public IActionResult GetDiscount(int id)
+        {
+            var value = _discountService.TGetById(id);
+            return Ok(value);
+        }
+
+        [HttpPut]
+
+        public IActionResult UpdateDiscount(UpdateDiscountDto updateDiscountDto)
+        {
+            _discountService.TUpdate(new Discount()
+            {
+                DiscountID = updateDiscountDto.DiscountID,
+                Title = updateDiscountDto.Title,
+                Amount = updateDiscountDto.Amount,
+                Description = updateDiscountDto.Description,
+                ImageUrl = updateDiscountDto.ImageUrl
+                
+            });
+            return Ok("Discount Updated");
+        }
+    }
+}
