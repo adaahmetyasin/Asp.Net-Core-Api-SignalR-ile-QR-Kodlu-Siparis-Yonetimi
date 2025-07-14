23b150d528799c9efdc8735d825af6d2e31fc9da
Yasin Ada <adaahmetyasinedu@gmail.com>
2025-07-14 23:57:35 +0300

    The basket class was integrated into the architecture.

---
+++ SignalR.BusinessLayer/Concrete/BasketManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DataAccessLayer.Abstract;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalR.BusinessLayer.Concrete
+{
+    public class BasketManager : IBasketService
+    {
+        private readonly IBasketDal _basketDal;
+        public BasketManager(IBasketDal basketDal)
+        {
+            _basketDal = basketDal;
+        }
+        public void TDelete(Basket t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Basket> TGetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Basket> TGetBasketsByMenuTableId(int menuTableId)
+        {
+            return _basketDal.GetBasketsByMenuTableId(menuTableId);
+        }
+
+        public Basket TGetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TInsert(Basket t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(Basket t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SignalR.DataAccessLayer/EntityFramework/EfBasketDal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SignalR.DataAccessLayer.Abstract;
+using SignalR.DataAccessLayer.Concrete;
+using SignalR.DataAccessLayer.Repositories;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalR.DataAccessLayer.EntityFramework
+{
+    public class EfBasketDal : GenericRepository<Basket>, IBasketDal
+    {
+        public EfBasketDal(SignalRContext context) : base(context)
+        {
+        }
+
+        public List<Basket> GetBasketsByMenuTableId(int menuTableId)
+        {
+            using var context = new SignalRContext();
+            return context.Baskets
+                .Where(b => b.MenuTableID == menuTableId)
+                .ToList();
+        }
+    }
+}
