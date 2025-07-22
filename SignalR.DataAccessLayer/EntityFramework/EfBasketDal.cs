558ac45df598d3a18e80cf584e49188084d12359
Yasin Ada <adaahmetyasinedu@gmail.com>
2025-07-22 15:55:26 +0300

    include method added for product name

--- SignalR.DataAccessLayer/EntityFramework/EfBasketDal.cs
+++ SignalR.DataAccessLayer/EntityFramework/EfBasketDal.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using SignalR.DataAccessLayer.Abstract;
 using SignalR.DataAccessLayer.Concrete;
 using SignalR.DataAccessLayer.Repositories;
@@ -21,6 +22,7 @@ namespace SignalR.DataAccessLayer.EntityFramework
             using var context = new SignalRContext();
             return context.Baskets
                 .Where(b => b.MenuTableID == menuTableId)
+                .Include(y => y.Product)
                 .ToList();
         }
     }
