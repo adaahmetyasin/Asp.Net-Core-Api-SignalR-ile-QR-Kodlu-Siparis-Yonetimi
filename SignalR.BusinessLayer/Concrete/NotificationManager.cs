6aa5fef9f82764d23a83a627b8abc01fc58f7441
Yasin Ada <adaahmetyasinedu@gmail.com>
2025-08-05 20:56:47 +0300

    notification sınıfı oluşturulup api ayarlandı

---
+++ SignalR.BusinessLayer/Concrete/NotificationManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DataAccessLayer.Abstract;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalR.BusinessLayer.Concrete
+{
+    public class NotificationManager : INotificationService
+    {
+        private readonly INotificationDal _notificationDal;
+        public NotificationManager(INotificationDal notificationDal)
+        {
+            _notificationDal = notificationDal;
+        }
+        public void TDelete(Notification t)
+        {
+            _notificationDal.Delete(t);
+        }
+
+        public List<Notification> TGetAll()
+        {
+            return _notificationDal.GetAll();
+        }
+
+        public Notification TGetById(int id)
+        {
+            return _notificationDal.GetById(id);
+        }
+
+        public void TInsert(Notification t)
+        {
+           _notificationDal.Insert(t);
+        }
+
+        public void TUpdate(Notification t)
+        {
+            _notificationDal.Update(t);
+        }
+    }
+}
