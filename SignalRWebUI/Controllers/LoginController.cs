15874fec26039736de0418c4a88f7c39f3de344d
Yasin Ada <adaahmetyasinedu@gmail.com>
2025-08-10 18:54:36 +0300

    login işlemleri yapıldı

---
+++ SignalRWebUI/Controllers/LoginController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.EntityLayer.Entities;
+using SignalRWebUI.Dtos.IdentityDtos;
+
+namespace SignalRWebUI.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public LoginController(SignInManager<AppUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Index(LoginDto loginDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(loginDto);
+            }
+            var result = await _signInManager.PasswordSignInAsync(loginDto.Username, loginDto.Password, false, false);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Category");
+            }
+            ModelState.AddModelError("", "Invalid login attempt.");
+            return View(loginDto);
+
+        }
+    }
+}
